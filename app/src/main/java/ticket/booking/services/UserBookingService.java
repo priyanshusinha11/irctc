9e8bc80a5fa4e27090c2c155d4aaf7fbbf19ca08
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2025-01-12 21:22:55 +0530

    user service

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,5 +1,23 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+
 public class UserBookingService {
+    private User user;
+    private List <User> userList;
+    private ObjectMapper objectMapper = new ObjectMapper()
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user1) throws IOException {
+        this.user  = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
 
 }
