439374b2a5132e6ea4dda1dafcda500676e994f0
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2025-01-12 21:48:39 +0530

    entites update

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -12,7 +12,7 @@ public class UserBookingService {
     private List <User> userList;
     private ObjectMapper objectMapper = new ObjectMapper()
 
-    private static final String USERS_PATH = "../localDb/users.json";
+    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
 
     public UserBookingService(User user1) throws IOException {
         this.user  = user1;
